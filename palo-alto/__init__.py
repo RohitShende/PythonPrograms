6033e38d27141a6ddc56779ea8d1bd633154c27e
rohitshende <rohitshende16@gmail.com>
2023-11-14 18:06:44 +0530

    palo alto interview

---
+++ palo-alto/__init__.py
@@ -0,0 +1,34 @@
+"""
+[2, 10, 10, 20, 20, 20, 20, 30, 30]
+ m
+s                               e
+
+0               4               8
+
+O(n)
+even number repititions
+find non-repeated number in Log(n)
+pairs are together
+but not sorted
+
+if (m-1 > 0 and arr[m] == arr[m-1])
+    left_count = m - s - 1
+else:
+    left_count = m - s
+
+
+if m-1 > 0 and m+1 < N:
+
+
+if  (m-1 > 0 and arr[m] != arr[m-1]) and (m-1 > 0 and m+1 < N and arr[m] != arr[m+1]):
+    # number found
+
+elif left_count % 2 == 0:
+    #search right
+else:
+    # search left
+
+"""
+
+
+
